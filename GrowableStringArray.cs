70c5513c94b7ee14429bb2d699d2ec60b74c4e60
Amanuel Workneh <mammo@rotselleri.com>
2011-03-24 16:23:38 +0100

    Extracted GrowableStringArray (which I'm not very fond of) from Luke.cs

---
+++ GrowableStringArray.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Lucene.Net.LukeNet
+{
+    public class GrowableStringArray
+    {
+        public int INITIAL_SIZE = 20;
+        private int size = 0;
+        private String[] array = null;
+
+        public int Size()
+        {
+            return size;
+        }
+
+        public void Set(int index, String val)
+        {
+            if (array == null) array = new String[INITIAL_SIZE];
+            if (array.Length < index + 1)
+            {
+                String[] newArray = new String[index + INITIAL_SIZE];
+                array.CopyTo(newArray, 0);
+                array = newArray;
+            }
+            if (index > size - 1) size = index + 1;
+            array[index] = val;
+        }
+
+        public String Get(int index)
+        {
+            return array[index];
+        }
+    }
+}
--- Luke.cs
+++ Luke.cs
@@ -1922,37 +1922,5 @@ namespace Lucene.Net.LukeNet
 		#endregion class ListViewItemComparer
 		
 	}
-
-	#region class GrowableStringArray
-	class GrowableStringArray 
-	{
-		public int INITIAL_SIZE = 20;
-		private int size = 0;
-		private String[] array = null;
-    
-		public int Size() 
-		{
-			return size;
-		}
-    
-		public void Set(int index, String val) 
-		{
-			if (array == null) array = new String[INITIAL_SIZE];
-			if (array.Length < index + 1) 
-			{
-				String[] newArray = new String[index + INITIAL_SIZE];
-				array.CopyTo(newArray, 0);
-				array = newArray;
-			}
-			if (index > size - 1) size = index + 1;
-			array[index] = val;
-		}
-    
-		public String Get(int index) 
-		{
-			return array[index];
-		}
-	}
-	#endregion class GrowableStringArray
 }
 
